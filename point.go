5c6d3713772f34432b0f418320aaf1edd2ca1bdb
Ola Bini <ola@olabini.se>
2019-04-09 09:26:24 -0500

    Rename some files

---
+++ point.go
@@ -0,0 +1,14 @@
+package goldilocks
+
+// We will do two interfaces - one that is functional, and one that is OO, based on the functional one
+
+// First functionality to put in:
+// - goldilocks_448_point_eq
+// - goldilocks_448_point_add
+// - goldilocks_448_point_double
+// - goldilocks_448_point_sub
+// - goldilocks_448_point_negate
+// - goldilocks_448_point_scalarmul
+// - goldilocks_448_point_double_scalarmul
+// - goldilocks_448_point_dual_scalarmul
+// - goldilocks_448_point_valid
