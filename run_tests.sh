8ee7ae9e67449575d7f3a880becf60371d49060e
Ola Bini <ola@olabini.se>
2019-04-10 11:36:53 -0500

    Set proper ulimit, and print our path

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,5 @@
 
 DIR=`pwd`
 
+echo $PATH
 sudo -E -i $DIR/run_tests_in.sh $DIR
--- run_tests_in.sh
+++ run_tests_in.sh
@@ -2,7 +2,7 @@
 
 echo "Running make test in $1"
 cd $1
-ulimit -a
-ulimit -l 1000
-ulimit -a
+ulimit -l 16384
+
+
 make test
